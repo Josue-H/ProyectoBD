0b6eeb1158fb462dcf5abba4f68020d0bc68ab75
Josue-H <josuecochoher@gmail.com>
2023-10-21 19:06:06 -0600

    segundo commit

--- providers/database.py
+++ providers/database.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 
 def init_db(app):
     #producción
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://tostadaconfrijol_SQLLogin_1:kd21j7pfk8@proyectobd.mssql.somee.com/proyectobd?driver=ODBC+Driver+17+for+SQL+Server'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://tostadaconfrijol_SQLLogin_1:kd21j7pfk8@proyectobd.mssql.somee.com/proyectobd?driver=ODBC+Driver+17+for+SQL+Server'
    #desarrollo
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://proyectod:12345@PC-TOSTADACONFR/proyectobd?driver=ODBC+Driver+17+for+SQL+Server'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://proyectod:12345@PC-TOSTADACONFR/proyectobd?driver=ODBC+Driver+17+for+SQL+Server'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
